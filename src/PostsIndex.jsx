e57c144d4ca4b730195c916a333d72e842f9b2fd
andytdietz <andytdietz@gmail.com>
2024-03-18 15:05:10 -0600

    fix formatting

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable react/prop-types */
 export function PostsIndex(props) {
   return (
-    <div className="cards" id="posts-index">
+    <div id="posts-index">
       <h1>All Posts</h1>
-      {props.posts.map((post) => (
-        <div className="card" key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          <img src={post.image} />
-          <button className="read-more-button" onClick={() => props.onShowPost(post)}>
-            Read More
-          </button>
-        </div>
-      ))}
+      <div className="cards">
+        {props.posts.map((post) => (
+          <div className="card" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <img src={post.image} />
+            <button className="read-more-button" onClick={() => props.onShowPost(post)}>
+              Read More
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
